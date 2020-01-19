786488e76f6f8f6fae27056f5beddb2d386a3cea
Alex Burner <alex.burner@gmail.com>
2020-01-18 16:54:10 -0800

    Reach parity with old site, except Flickity

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,23 @@
 import React from "react";
-import { NextPageContext } from "next";
 
-interface Props {
-  userAgent?: string;
-}
+import { Brand } from "../components/home/Brand";
+import { About } from "../components/home/About";
+import { Skills } from "../components/home/Skills";
+import { Work } from "../components/home/Work";
+import { Articles } from "../components/home/Articles";
+import { Contact } from "../components/home/Contact";
 
-export default class IndexPage extends React.Component<Props> {
-  static async getInitialProps({ req }: NextPageContext) {
-    const userAgent = req ? req.headers["user-agent"] : navigator.userAgent;
-    return { userAgent };
-  }
-
+export default class IndexPage extends React.Component<{}> {
   render() {
-    const { userAgent } = this.props;
     return (
-      <div>
-        <h1>Your user agent: {userAgent}</h1>
-        <section className="skills section">
-          <div className="column is-4">
-            <p className="title is-3">
-              <span className="fa-stack fa-2x">
-                <i className="fas fa-circle fa-stack-2x"></i>
-                <i className="fas fa-crosshairs fa-stack-1x"></i>
-              </span>
-            </p>
-          </div>
-        </section>
-      </div>
+      <>
+        <Brand />
+        <About />
+        <Skills />
+        <Work />
+        <Articles />
+        <Contact />
+      </>
     );
   }
 }
