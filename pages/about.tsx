0b84149d9d1e0a45aa14f8ae38abd80a6d50b1c6
Alex Burner <alex.burner@gmail.com>
2020-02-17 11:36:52 -0800

    Remove leftover console.logs

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,9 +4,7 @@ import { RichText } from "prismic-reactjs";
 import { getPage, Page } from "../util/prismic";
 
 const About: NextPage<{ page: Page }> = ({ page }) => {
-  console.log(page.body);
   const html = RichText.render(page.body);
-  console.log(html);
   return <>{html}</>;
 };
 
